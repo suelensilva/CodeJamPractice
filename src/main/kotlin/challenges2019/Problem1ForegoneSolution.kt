5beaadab4b17bb380131ae557e776731f9427b15
Suelen <suelen.apsilva@gmail.com>
2021-03-21 20:19:27 -0300

    Solution to problem 1 of code jam 2019

--- src/main/kotlin/challenges2019/Problem1ForegoneSolution.kt
+++ src/main/kotlin/challenges2019/Problem1ForegoneSolution.kt
@@ -1,25 +1,33 @@
 package challenges2019
 
-import kotlin.math.pow
+import java.math.BigDecimal
 
 fun main(args: Array<String>) {
     val nTests = readLine()!!.toInt()
     for(i in 1..nTests) {
-        val n = readLine()!!.toInt()
+        val n = readLine()!!.toBigDecimal()
         val a = calculateA(n)
         val b = n - a
         println("Case #$i: $a $b")
     }
 }
 
-private fun calculateA(n: Int): Int {
-    var sumA = 0
+private fun calculateA(n: BigDecimal): BigDecimal {
+    var sumA = BigDecimal(0)
     val nStr = n.toString()
     for(i in nStr.length - 1 downTo 0) {
         val d = Character.getNumericValue(nStr[i])
         if(d == 4) {
-            sumA += 10.0.pow((nStr.length - 1 - i).toDouble()).toInt()
+            sumA += calculateDecimal(nStr.length - 1 - i)
         }
     }
     return sumA
+}
+
+private fun calculateDecimal(decimal: Int): BigDecimal {
+    var result = "1"
+    for(int in 1 .. decimal) {
+        result += 0
+    }
+    return result.toBigDecimal()
 }
