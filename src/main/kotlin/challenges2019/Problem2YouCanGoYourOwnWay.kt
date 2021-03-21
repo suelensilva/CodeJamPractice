3ba002583e7e28c911a3f963d764e4ace1af10ae
Suelen <suelen.apsilva@gmail.com>
2021-03-21 20:43:13 -0300

    Solution to problem 2 of code jam 2019

---
+++ src/main/kotlin/challenges2019/Problem2YouCanGoYourOwnWay.kt
@@ -0,0 +1,15 @@
+package challenges2019
+
+fun main(args: Array<String>) {
+    val nTests = readLine()!!.toInt()
+    for(i in 1..nTests) {
+        val n = readLine()!!.toInt()
+        val lidiaPath = readLine()!!
+        val myPath = lidiaPath.map {
+            if(it == 'E') 'S' else 'E'
+        }
+        print("Case #$i: ")
+        myPath.forEach { print(it) }
+        println()
+    }
+}
